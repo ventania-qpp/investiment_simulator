d1340b47cb6ea324146d5e3864bc18d93a8542c4
ventania-qpp <ventania.app0122@gmail.com>
2026-04-19 22:14:27 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import japanize_matplotlib
 
-
+plt.rcParams["font.family"] = "IPAexGothic"
 #plt.rcParams["font.family"] = "MS Gothic"
 
 st.set_page_config(page_title="積立シミュレーター", layout="wide")
